ad9b28a6d85cab9a22075ffc8333c0eaa25269a1
Jack <jackmccaffrey96@gmail.com>
2021-08-11 17:29:33 +0100

    Updated ML_01 Level Mesh

--- src/global/global_flags.gd
+++ src/global/global_flags.gd
@@ -3,6 +3,6 @@ extends Node
 var PLAYER_CONTROLS_ACTIVE = true
 var CAMERA_INVERT_X = false
 var CAMERA_INVERT_Y = true
-var SHOW_STATE_LABELS = false
-var SHOW_DEBUG_TRAJECTORIES = false
+var SHOW_STATE_LABELS = true
+var SHOW_DEBUG_TRAJECTORIES = true
 
