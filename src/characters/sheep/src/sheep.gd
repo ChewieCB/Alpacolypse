f745f1c99be810c3c6f3e0bfbf0afd9816301bcd
Jack <jackmccaffrey96@gmail.com>
2021-08-18 16:33:51 +0100

    Expanded Sheep Wander Range

--- src/characters/sheep/src/sheep.gd
+++ src/characters/sheep/src/sheep.gd
@@ -1,9 +1,10 @@
 extends KinematicBody
+class_name Sheep
 
 onready var state_machine = $StateMachine
 onready var state_label = $StatusLabels/Viewport/StateLabel
 onready var skin = $SheepSkin
 onready var collider = $CollisionShape
 
-export (int, 2, 100) var wander_range = 25
+export (int, 2, 100) var wander_range = 50
 
