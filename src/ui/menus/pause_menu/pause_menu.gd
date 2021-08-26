3e5f70ec2e825613c616d35d8718eec272e56dd7
Jack <jackmccaffrey96@gmail.com>
2021-08-26 20:45:26 +0100

    Connected Resume and Quit Buttons In Pause Menu

--- src/ui/menus/pause_menu/pause_menu.gd
+++ src/ui/menus/pause_menu/pause_menu.gd
@@ -4,5 +4,27 @@ extends Control
 
 func _input(_event):
 	if Input.is_action_just_pressed("pause"):
-		get_tree().paused = !get_tree().paused
-		self.visible = !self.visible
+		toggle_pause_menu()
+
+
+func toggle_pause_menu():
+	get_tree().paused = !get_tree().paused
+	self.visible = !self.visible
+	
+	if self.visible and get_tree().paused:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _on_ResumeButton_pressed():
+	toggle_pause_menu()
+
+
+func _on_OptionsButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+	# TODO - make this go back to main menu
