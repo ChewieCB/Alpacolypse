cd12db24315060f230db7338c57450794208b3bd
Jack <jackmccaffrey96@gmail.com>
2021-09-01 21:21:51 +0100

    Added Windowed/Fullscreen Toggle To Menus

--- src/ui/menus/main_menu/main_menu.gd
+++ src/ui/menus/main_menu/main_menu.gd
@@ -4,10 +4,22 @@ onready var camera = $Camera
 onready var llama = $Llama
 onready var fadeout = $UI/Fadeout
 
+onready var windowed_button = $UI/Control/VBoxContainer2/HBoxContainer/VBoxContainer/CenterContainer2/HBoxContainer/WindowedButton
+onready var fullscreen_button = $UI/Control/VBoxContainer2/HBoxContainer/VBoxContainer/CenterContainer2/HBoxContainer/FullscreenButton
+
 
 func _ready():
+	get_tree().paused = false
 	fadeout.fade_in()
 	llama.transition_to(llama.States.IDLE2)
+	
+	# Windowed/fullscreen set
+	if GlobalFlags.FULLSCREEN:
+		windowed_button.disabled = false
+		fullscreen_button.disabled = true
+	else:
+		windowed_button.disabled = true
+		fullscreen_button.disabled = false
 
 
 func transition_to_game():
@@ -25,3 +37,17 @@ func _on_QuitButton_pressed():
 #	yield(llama.animation_player, "animation_changed")
 	yield(fadeout.fade_out(), "completed")
 	get_tree().quit()
+
+
+func _on_WindowedButton_pressed():
+	windowed_button.disabled = true
+	fullscreen_button.disabled = false
+	GlobalFlags.set_FULLSCREEN(false)
+
+
+func _on_FullscreenButton_pressed():
+	windowed_button.disabled = false
+	fullscreen_button.disabled = true
+	GlobalFlags.set_FULLSCREEN(true)
+
+
