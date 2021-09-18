1d3c7e5d645433c077adb31940b9e2dc31a3cb24
Jack <jackmccaffrey96@gmail.com>
2021-09-18 16:13:55 +0100

    Added SplashParticles that spawn In when entering a DrowningZone
    Currently isn't appearing for sheep, but works for player just fine.

--- src/dev_zones/drowning/drowning_zone.gd
+++ src/dev_zones/drowning/drowning_zone.gd
@@ -1,5 +1,8 @@
 extends Area
 
+onready var splash_particles_scene = load("res://src/particles/SplashParticles.tscn")
+var splash
+
 
 func _ready():
 	# Hide dev zone meshes in-game
@@ -8,8 +11,20 @@ func _ready():
 
 
 func _on_DrowningZone_body_entered(body):
+	# Generate a splash particles scene at the body's position
+	splash = splash_particles_scene.instance()
+	body.add_child(splash)
+	splash.global_transform.origin = body.global_transform.origin
+	splash.global_transform.origin.y += 2
+	splash.emitting = true
+	
 	if body is PlayerController:
 		body.state_machine.transition_to("Movement/Drowning")
 	else:
 		# Call the body's respawn method
 		body.respawn()
+
+
+func _on_DrowningZone_body_exited(_body):
+	if splash:
+		splash.queue_free()
